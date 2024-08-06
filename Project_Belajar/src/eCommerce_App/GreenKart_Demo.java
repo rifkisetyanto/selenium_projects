e5ee8b720855cefd80cff219fcc8960c8ef7c88b
Laptop Pribadi Rifki <rifkisetyanto@gmail.com>
2024-08-06 16:23:58 +0700

    Added greenKart

--- Project_Belajar/src/eCommerce_App/GreenKart_Demo.java
+++ Project_Belajar/src/eCommerce_App/GreenKart_Demo.java
@@ -1,5 +1,9 @@
 package eCommerce_App;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GreenKart_Demo {
@@ -7,8 +11,19 @@ public class GreenKart_Demo {
 	public static void main(String[] args) {
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 		
-
+		for(int i = 0; i < products.size(); i++) {
+			
+			//memvalidasi teks nya sampai menemukan teks yang sesuai dengan yang kita mau
+			String nama = products.get(i).getText();
+			
+			if(nama.contains("Cucumber")) {
+				// klik add To Cart
+				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+				break;
+			}
+		}
 	}
 
 }
