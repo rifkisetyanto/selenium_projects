2ac8399136ca305850a3527724a490b77fd6423c
Laptop Pribadi Rifki <rifkisetyanto@gmail.com>
2024-08-07 21:26:50 +0700

    Added ArrayList

--- Project_Belajar/src/eCommerce_App/GreenKart_Demo.java
+++ Project_Belajar/src/eCommerce_App/GreenKart_Demo.java
@@ -1,5 +1,6 @@
 package eCommerce_App;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -12,18 +13,28 @@ public class GreenKart_Demo {
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+		//array == item dari yagn mau kita pilih.
+		String namaItem [] = {"Cucumber - 1kg","Beetroot - 1kg","Tomato"};
+		
 		
 		for(int i = 0; i < products.size(); i++) {
 			
 			//memvalidasi teks nya sampai menemukan teks yang sesuai dengan yang kita mau
 			String nama = products.get(i).getText();
-			
-			if(nama.contains("Cucumber")) {
+			/**
+			 * split dan trim 
+			 * trim == menghilangkan spasi
+			 * split == memisahkan teks atau karakter yang kita inginkan
+			 * 
+			 */
+			List<String> listItem = Arrays.asList(namaItem);
+			if(listItem.contains(nama)) {
 				// klik add To Cart
 				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-				break;
 			}
+			
 		}
 	}
 
 }
+1
