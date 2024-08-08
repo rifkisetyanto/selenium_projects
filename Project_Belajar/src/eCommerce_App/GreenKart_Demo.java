a8f736fd7175471291dd39ce69f1c8c324019b1e
Laptop Pribadi Rifki <rifkisetyanto@gmail.com>
2024-08-08 13:34:47 +0700

    Added Array greenKart

--- Project_Belajar/src/eCommerce_App/GreenKart_Demo.java
+++ Project_Belajar/src/eCommerce_App/GreenKart_Demo.java
@@ -12,29 +12,25 @@ public class GreenKart_Demo {
 	public static void main(String[] args) {
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-		//array == item dari yagn mau kita pilih.
-		String namaItem [] = {"Cucumber - 1kg","Beetroot - 1kg","Tomato"};
-		
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));	
+		String[] namaItem = {"Cucumber","Tomato","Brocolli"};
 		
 		for(int i = 0; i < products.size(); i++) {
 			
 			//memvalidasi teks nya sampai menemukan teks yang sesuai dengan yang kita mau
 			String nama = products.get(i).getText();
 			/**
-			 * split dan trim 
-			 * trim == menghilangkan spasi
-			 * split == memisahkan teks atau karakter yang kita inginkan
-			 * 
+			 * Eliminasi pada teks yang didapatkan oleh selenium
+			 * Trim == spasi nya
+			 * SPlit == teks nya
 			 */
 			List<String> listItem = Arrays.asList(namaItem);
+			
 			if(listItem.contains(nama)) {
 				// klik add To Cart
 				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
 			}
-			
 		}
 	}
 
 }
-1
