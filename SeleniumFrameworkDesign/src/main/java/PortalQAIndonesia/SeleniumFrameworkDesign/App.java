ce522600a29658ec467381cd65a1cf07c7fb8efb
Laptop Pribadi Rifki <rifkisetyanto@gmail.com>
2024-12-15 20:38:26 +0700

    added new project Selenium Framework Design

---
+++ SeleniumFrameworkDesign/src/main/java/PortalQAIndonesia/SeleniumFrameworkDesign/App.java
@@ -0,0 +1,10 @@
+package PortalQAIndonesia.SeleniumFrameworkDesign;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
